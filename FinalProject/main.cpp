531b9de2bc2c4fe877c5cece6dfc7e9d6b12fcf0
John-Morgan Barnhart <jbarnh17@uncc.edu>
2018-04-29 17:56:51 -0400

    Initial Commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by John-Morgan Barnhart on 4/29/18.
+//  Copyright © 2018 John-Morgan Barnhart. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
